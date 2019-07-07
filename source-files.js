e9c03141591be964613aabd353792f91552efc88
Travis CI User <travis@example.org>
2019-07-07 10:09:30 +0000

    Documentation for Amanieu/intrusive-rs@741bb6c

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["intrusive_collections"] = {"name":"","files":["adapter.rs","intrusive_pointer.rs","key_adapter.rs","lib.rs","linked_list.rs","rbtree.rs","singly_linked_list.rs","unsafe_ref.rs"]};
+createSourceSidebar();
